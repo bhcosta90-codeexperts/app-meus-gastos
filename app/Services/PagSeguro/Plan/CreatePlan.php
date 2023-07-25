05ad60b3df05e866d3644b095fbd21d9d0394e07
Bruno Costa <bruno.costa@dafiti.com.br>
2023-07-25 09:52:04 -0300

    finalizado

--- app/Services/PagSeguro/Plan/CreatePlan.php
+++ app/Services/PagSeguro/Plan/CreatePlan.php
@@ -2,30 +2,33 @@
 
 namespace App\Services\PagSeguro\Plan;
 
+use App\Services\PagSeguro\Credentials;
 use Illuminate\Support\Facades\Http;
 
 class CreatePlan
 {
     public static function makeRequest(array $data)
     {
-        $response = Http::timeout(5)->withHeaders([
-            'Accept' => 'application/json',
-            'Content-Type' => 'application/json',
-            'Authorization' => config('pagseguro.token')
+        $url = Credentials::getCredentials('/pre-approvals/request/');
+
+        $response = Http::timeout(10)->withHeaders([
+            'Accept' => 'application/vnd.pagseguro.com.br.v3+json;charset=ISO-8859-1',
+            'Content-Type' => 'application/json'
         ])->post(
-            'https://sandbox.api.assinaturas.pagseguro.com/plans',
+            $url,
             [
-                "amount" => [
-                    "value" => (float) $data['price'],
-                    "currency" => "BRL"
-                ],
-                "interval" => [
-                    "unit" => "MONTH"
-                ],
-                "name" => $data['slug']
+                'reference' => $data['slug'],
+                'preApproval' => [
+                    'name' => $data['name'],
+                    'charge' => 'AUTO',
+                    'period' => 'MONTHLY',
+                    'amountPerPayment' => 3999
+                ]
             ]
         );
 
-        return $response->json()['id'];
+        \Log::info($response->json());
+
+        return $response->json()['code'];
     }
 }
