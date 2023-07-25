47e7140003aa17a17707c6d1038e0bf281bb57d3
Bruno Costa <bruno.costa@dafiti.com.br>
2023-07-25 10:58:01 -0300

    criado o componente de cartão de crédito

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,43 +3,31 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>Laravel</title>
 
         <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
         <!-- Styles -->
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
+
+        <style>
+            body {
+                font-family: 'Nunito';
+            }
+        </style>
+
         @livewireStyles
+
     </head>
-    <body class="font-sans antialiased">
-        <x-banner />
+    <body class="antialiased">
+        <div class="relative flex justify-center min-h-screen bg-gray-100 items-top dark:bg-gray-900 sm:items-center sm:pt-0">
 
-        <div class="min-h-screen bg-gray-100">
-            @livewire('navigation-menu')
+            {{$slot}}
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
-
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+            @livewireScripts
         </div>
-
-        @stack('modals')
-
-        @livewireScripts
     </body>
+
 </html>
